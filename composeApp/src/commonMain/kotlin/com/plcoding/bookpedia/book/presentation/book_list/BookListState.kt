c412a8fad69853ae43c97ae101ac89e761c8d223
pramodaya <pramodayajayalath@gmail.com>
2025-05-11 18:43:05 +0200

    Added look list initial code

---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListState.kt
@@ -0,0 +1,14 @@
+package com.plcoding.bookpedia.book.presentation.book_list
+
+import com.plcoding.bookpedia.book.domain.Book
+import com.plcoding.bookpedia.core.presentation.UiText
+import org.koin.core.logger.MESSAGE
+
+data class BookListState(
+    val searchQuery: String = "Kotlin",
+    val searchResults: List<Book> = emptyList(),
+    val favoriteBooks: List<Book> = emptyList(),
+    val isLoading: Boolean = false,
+    val selectedTabIndex: Int = 0,
+    val errorMessage: UiText? = null
+)
